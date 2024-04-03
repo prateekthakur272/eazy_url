4e0230fbd91437207254b904412f5e42b214d39d
Prateek Thakur <prateekthakur272@gmail.com>
2024-04-03 21:58:00 +0530

    adds database and settings

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from settings import get_settings
+
+
+settings = get_settings()
+engine = create_engine(settings.DB_URL, echo=True, pool_pre_ping=True, pool_recycle=300, pool_size=5, max_overflow=0)
+
+Base = declarative_base()
+
+Session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+def get_db_session():
+    session = Session()
+    try:
+        yield session
+    finally:
+        session.close()
---
+++ settings.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DB_URL:str = 'sqlite:///db.sqlite3'
+
+
+def get_settings() -> Settings:
+    return Settings()
